629a82bfa93c533fe1454ec1670500fc216e2059
Wind <>
2022-04-11 02:46:05 +0800

    fix bug

--- lib/net/wallet_services.dart
+++ lib/net/wallet_services.dart
@@ -304,8 +304,7 @@ class WalletServices {
     return [];
   }
 
-  static Future<List<MCollectionTokens>> getUserNftList(
-      {required String address}) async {
+  static Future<List<dynamic>> getUserNftList({required String address}) async {
     final url = RequestURLS.getHost() + RequestURLS.userNftList;
     Map<String, dynamic>? params = {};
     params["address"] = address;
@@ -313,38 +312,72 @@ class WalletServices {
         .requestData(Method.GET, url, queryParameters: params);
     if (result != null && result["code"] == 200) {
       List data = result["result"]["page"] ?? [];
-      List<MCollectionTokens> values = [];
-      for (var item in data) {
-        String chainTypeName = item["chainTypeName"];
-        String contractAddress = item["contractAddress"];
-        String id = item["nftId"].toString();
-        String token = item["contract_name"] ?? "";
-        String url = item["url"];
-        String nftTypeName = item["nftTypeName"];
-        Decimal sumCount =
-            Decimal.tryParse(item["sumCount"].toString()) ?? Decimal.zero;
 
-        // contractAddress = "0x2c30b60038394755c9ab4285fa59d722153a63e5";
-        // id = "446160950877979480142007162702782189984542661413";
-        // sumCount = Decimal.fromInt(10);
-        // chainTypeName = "ETH";
+      // data.addAll([
+      //   {
+      //     "chainTypeName": "eth",
+      //     "contractAddress": "0x064e16771A4864561f767e4Ef4a6989fc4045aE7",
+      //     "id": 2,
+      //     "nftId": [78932],
+      //     "nftTypeName": "721",
+      //     "sumCount": 0,
+      //   },
+      //   {
+      //     "chainTypeName": "eth",
+      //     "contractAddress": "0xbf3181c23f25cc8ad5d326a3a313f80e9162c8d2",
+      //     "id": 2,
+      //     "nftId": [100],
+      //     "nftTypeName": "721",
+      //     "sumCount": 0,
+      //   },
+      //   {
+      //     "chainTypeName": "eth",
+      //     "contractAddress": "0x4b2e42c23c7a85ff7874cbdad65b3421e5197c76",
+      //     "id": 2,
+      //     "nftId": [0],
+      //     "nftTypeName": "721",
+      //     "sumCount": 0,
+      //   },
+      //   {
+      //     "chainTypeName": "eth",
+      //     "contractAddress": "0x064e16771A4864561f767e4Ef4a6989fc4045aE7",
+      //     "id": 2,
+      //     "nftId": [78932],
+      //     "nftTypeName": "721",
+      //     "sumCount": 0,
+      //   }
+      // ]);
 
-        MCollectionTokens model = MCollectionTokens();
-        model.contract = contractAddress;
-        model.digits = 0;
-        model.chainType = chainTypeName.chainTypeGetCoinType()!.index;
-        model.tid = id;
-        model.iconPath = url;
-        model.decimals = 0;
-        model.token = token;
-        model.tokenType = nftTypeName == "721"
-            ? KTokenType.eip721.index
-            : KTokenType.eip1155.index;
-        model.balance = sumCount.toDouble();
-        model.coinType = chainTypeName.chainTypeGetCoinType()!.coinTypeString();
-        values.add(model);
-      }
-      return values;
+      // String chainTypeName = item["chainTypeName"];
+      // String contractAddress = item["contractAddress"];
+      // String id = item["nftId"].toString();
+      // String token = item["contract_name"] ?? "";
+      // String url = item["url"];
+      // String nftTypeName = item["nftTypeName"];
+      // Decimal sumCount =
+      //     Decimal.tryParse(item["sumCount"].toString()) ?? Decimal.zero;
+
+      // contractAddress = "0x2c30b60038394755c9ab4285fa59d722153a63e5";
+      // id = "446160950877979480142007162702782189984542661413";
+      // sumCount = Decimal.fromInt(10);
+      // chainTypeName = "ETH";
+
+      // MCollectionTokens model = MCollectionTokens();
+      // model.contract = contractAddress;
+      // model.digits = 0;
+      // model.chainType = chainTypeName.chainTypeGetCoinType()!.index;
+      // model.tid = id;
+      // model.iconPath = url;
+      // model.decimals = 0;
+      // model.token = token;
+      // model.tokenType = nftTypeName == "721"
+      //     ? KTokenType.eip721.index
+      //     : KTokenType.eip1155.index;
+      // model.balance = sumCount.toDouble();
+      // model.coinType = chainTypeName.chainTypeGetCoinType()!.coinTypeString();
+      // values.add(model);
+
+      return data;
     }
     return [];
   }
